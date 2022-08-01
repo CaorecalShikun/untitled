0925421115444611371340b812cd052bab8afba3
recao <recao@deloitte.com.cn>
2022-08-01 15:44:21 +0800

    commit 1

--- src/recao1.java
+++ src/recao1.java
@@ -2,5 +2,7 @@ public class recao1 {
     public static void main(String[] args) {
         int o=10;
         System.out.print(o/2);
+        String name = "dawdwd";
+        System.out.println(name.toLowerCase());
     }
 }
